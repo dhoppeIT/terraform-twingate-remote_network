a7955011d707a44eb6bcc784a64149ab3bdec38a
Dennis Hoppe <dennis.hoppe@dhoppe.it>
2026-02-24 08:26:41 +0100

    fix(terraform): Add missing input variable validation

--- variables.tf
+++ variables.tf
@@ -7,10 +7,20 @@ variable "location" {
   type        = string
   default     = null
   description = "The location of the Remote Network"
+
+  validation {
+    condition     = var.location == null || contains(["AWS", "AZURE", "GOOGLE_CLOUD", "ON_PREMISE", "OTHER"], var.location)
+    error_message = "Location must be one of: AWS, AZURE, GOOGLE_CLOUD, ON_PREMISE, OTHER"
+  }
 }
 
 variable "type" {
   type        = string
   default     = "REGULAR"
   description = "The type of the Remote Network"
+
+  validation {
+    condition     = contains(["REGULAR", "EXIT"], var.type)
+    error_message = "Type must be one of: REGULAR, EXIT"
+  }
 }
